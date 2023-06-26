1baae7ff1c0363affc6de6230ab730bf294be42f
Ch3my <joseconchan1@gmail.com>
2023-06-26 18:31:29 -0400

    Init
    Mostrando la escala menor y mayor. Faltaria incluir los acordes para cada caso y unas notas sobre harmonizaciones comunes que se pueden aplicar a cualquier escala

---
+++ scales.js
@@ -0,0 +1,64 @@
+const scales = [
+    {
+        name: 'C',
+        major: ['C', 'D', 'E', 'F', 'G', 'A', 'B'],
+        minor: ['C', 'D', 'D#', 'F', 'G', 'G#', 'A#'],
+    },
+    {
+        name: 'C#',
+        major: ['C#', 'D#', 'F', 'F#', 'G#', 'A#', 'C'],
+        minor: ['C#', 'D#', 'E', 'F#', 'G#', 'A', 'B'],
+    },
+    {
+        name: 'D',
+        major: ['D', 'E', 'F#', 'G', 'A', 'B', 'C#'],
+        minor: ['D', 'E', 'F', 'G', 'A', 'A#', 'C'],
+    },
+    {
+        name: 'D#',
+        major: ['D#', 'F', 'G', 'G#', 'A#', 'C', 'D'],
+        minor: ['D#', 'F', 'F#', 'G#', 'A#', 'B', 'C#'],
+    },
+    {
+        name: 'E',
+        major: ['E', 'F#', 'G#', 'A', 'B', 'C#', 'D#'],
+        minor: ['E', 'F#', 'G', 'A', 'B', 'C', 'D'],
+    },
+    {
+        name: 'F',
+        major: ['F', 'G', 'A', 'A#', 'C', 'D', 'E'],
+        minor: ['F', 'G', 'G#', 'A#', 'C', 'C#', 'D#'],
+    },
+    {
+        name: 'F#',
+        major: ['F#', 'G#', 'A#', 'B', 'C#', 'D#', 'F'],
+        minor: ['F#', 'G#', 'A', 'B', 'C#', 'D', 'E'],
+    },
+    {
+        name: 'G',
+        major: ['G', 'A', 'B', 'C', 'D', 'E', 'F#'],
+        minor: ['G', 'A', 'A#', 'C', 'D', 'D#', 'F'],
+    },
+    {
+        name: 'G#',
+        major: ['G#', 'A#', 'C', 'C#', 'D#', 'F', 'G'],
+        minor: ['G#', 'A#', 'B', 'C#', 'D#', 'E', 'F#'],
+    },
+    {
+        name: 'A',
+        major: ['A', 'B', 'C#', 'D', 'E', 'F#', 'G#'],
+        minor: ['A', 'B', 'C', 'D', 'E', 'F', 'G'],
+    },
+    {
+        name: 'A#',
+        major: ['A#', 'C', 'D', 'D#', 'F', 'G', 'A'],
+        minor: ['A#', 'C', 'C#', 'D#', 'F', 'F#', 'G#'],
+    },
+    {
+        name: 'B',
+        major: ['B', 'C#', 'D#', 'E', 'F#', 'G#', 'A#'],
+        minor: ['B', 'C#', 'D', 'E', 'F#', 'G', 'A'],
+    },
+];
+
+export default scales;
